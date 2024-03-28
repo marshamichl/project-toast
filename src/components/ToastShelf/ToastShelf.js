a52c29adf43b4a4a74f743d271828edb1b8a6b5e
Marsha Michl <marsha@fake-email.com>
2024-03-28 15:38:11 +0100

    Render dismissable toasts from list

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -3,17 +3,41 @@ import React from 'react';
 import Toast from '../Toast';
 import styles from './ToastShelf.module.css';
 
-function ToastShelf() {
-  return (
-    <ol className={styles.wrapper}>
-      <li className={styles.toastWrapper}>
-        <Toast variant="notice">Example notice toast</Toast>
-      </li>
-      <li className={styles.toastWrapper}>
-        <Toast variant="error">Example error toast</Toast>
-      </li>
-    </ol>
-  );
+function ToastShelf({ toasts, setToastMessages }) {
+	React.useEffect(() => {
+		function handleKeyDown(event) {
+			if (event.code === 'Escape') {
+				setToastMessages([]);
+			}
+		}
+
+		window.addEventListener('keydown', handleKeyDown);
+
+		return () => {
+			window.removeEventListener('keydown', handleKeyDown);
+		};
+	}, [setToastMessages]);
+
+	function handleDismissToast(id) {
+		const newToastList = toasts.filter((toast) => toast.id !== id);
+		setToastMessages(newToastList);
+	}
+
+	return (
+		<ol className={styles.wrapper}>
+			{toasts.length > 0 &&
+				toasts.map((toast) => (
+					<li key={toast.id} className={styles.toastWrapper}>
+						<Toast
+							handleDismiss={() => handleDismissToast(toast.id)}
+							variant={toast.variant}
+						>
+							{toast.message}
+						</Toast>
+					</li>
+				))}
+		</ol>
+	);
 }
 
 export default ToastShelf;
